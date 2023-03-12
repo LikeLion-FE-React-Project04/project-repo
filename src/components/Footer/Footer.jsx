2f5e541f0d547a30d641f29bc5fb7c7bffebe65c
SeoMiYoung <seo20seo00@naver.com>
2023-03-12 13:16:06 +0900

    ✨ [feat] footerCopyrightWrap 부분 마크업 및 CSS #10

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -43,9 +43,14 @@ export function Footer() {
             </p>
           </button>
         </section>
-        {/* <section className={styles.footerCopyrightWrap}>
-
-        </section> */}
+        <section className={styles.footerCopyrightWrap}>
+          <p>
+            마켓컬리에서 판매되는 상품 중에는 마켓컬리에 입점한 개별 판매자가 판매하는 마켓플레이스(오픈마켓) 상품이 포함되어 있습니다.<br/>
+            마켓플레이스(오픈마켓) 상품의 경우 컬리는 통신판매중개자로서 통신판매의 당사자가 아닙니다. 
+            컬리는 해당 상품의 주문, 품질, 교환/환불 등 의무와 책임을 부담하지 않습니다.
+          </p>
+          <p className={styles.copyrightEnglishText}>© KURLY CORP. ALL RIGHTS RESERVED</p>
+        </section>
       </footer>
     </>
   );
