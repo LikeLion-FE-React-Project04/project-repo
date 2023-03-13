d098ad7d96b1b1b24208c9fdb9a1ab77e94af32c
PracticeKJY <poiijtl1004@naver.com>
2023-03-14 02:57:20 +0900

    ✨ [feat]검색창 레이아웃 구성 추가#62

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,3 +1,4 @@
 export { default as Topbanner } from './Topbanner/Topbanner';
 export { default as Member } from './Member/Member';
 export { default as HeaderLogoContainer } from './HeaderLogoContainer/HeaderLogoContainer';
+export { default as SearchInput } from './SearchInput/SearchInput';
