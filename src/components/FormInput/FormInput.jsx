931808f1f29fac0f39074cb75a2800f9370b09ee
= <=>
2023-03-13 12:56:37 +0900

    🧑 [QUESTION] 컴포넌트 범용성 높이기 #50

--- src/components/FormInput/FormInput.jsx
+++ src/components/FormInput/FormInput.jsx
@@ -1,9 +1,41 @@
-import styles from "./FormInput.module.scss";
+import styles from './FormInput.module.scss';
 
-export function FormInput(props) {
+export function FormInput({
+  uiType = 'primary',
+  className = '',
+  customStyle = {},
+  ...restProps
+}) {
+  console.log('className', className);
+  const combinedClassNames = `${styles.input} ${className} ${getUiStyle(
+    uiType
+  )}`.trim();
+
+  console.log('customStyle', customStyle);
+
   return (
-    <div className={styles.FormInput}>
-      <input id="" name={props.name} placeholder={props.placeholder} type={props.type} />
-    </div>
-  )
+    <input {...restProps} className={`${styles.input}`} style={customStyle} />
+  );
 }
+
+function getUiStyle(uiType) {
+  let uiStyle;
+
+  switch (uiType) {
+    case 'primary':
+      uiStyle = styles.primary;
+      break;
+    case 'secondary':
+      uiStyle = styles.secondary;
+      break;
+  }
+
+  return uiStyle;
+}
+
+// props = {className:{styles.test2}
+//         label:"이메일"
+//         name:"email"
+//         placeholder:"아이디를 입력해주세요"
+//         type:"email"
+//       uiType="secondary"}
