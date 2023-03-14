aaa114fa09563b2497cc46bd00077963e36762f2
PracticeKJY <poiijtl1004@naver.com>
2023-03-14 15:30:58 +0900

    ✨ [feat] 추가저장커밋 #70

--- src/components/Header/HeaderNav/HeaderNav.jsx
+++ src/components/Header/HeaderNav/HeaderNav.jsx
@@ -142,11 +142,6 @@ const useScollEvent = () => {
 function HeaderNav() {
   const { testBorder, deliveryBtn, mainIcons, searchForm } = useScollEvent();
 
-  // const testBorder = useRef(null);
-  // const deliveryBtn = useRef(null);
-  // const mainIcons = useRef(null);
-  // const searchForm = useRef(null);
-
   // const categoryData = [
   //   { title: '선물하기', imgSrc: 'assets/header/ic-gift.svg' },
   //   { title: '채소', imgSrc: 'assets/header/ic-vegetable.svg' },
@@ -171,103 +166,6 @@ function HeaderNav() {
   //   { title: '베이비ㆍ키즈ㆍ완구', imgSrc: 'assets/header/ic-baby.svg' },
   //   { title: '여행ㆍ티켓', imgSrc: 'assets/header/ic-travel.svg' },
   // ]
-
-  //---------------------------------------------------------
-  // useEffect(() => {
-  //   //1.구독
-  //   const test1 = document.getElementById('1');
-
-  //   window.addEventListener('scroll', () => {
-  //     if (window.scrollY > 190) {
-  //       deliveryBtn.current.style = `display:none`;
-  //       mainIcons.current.style = `display:block`;
-  //       searchForm.current.style = `display:block`;
-
-  //       testBorder.current.style = `position:fixed;
-  //       left: 50%;
-  //       width:100%;
-  //       height:66.39px;
-  //       top:0;
-  //       transform: translateX(-50%);
-  //       box-shadow: 0rem 0.25rem 1.5rem rgba(0, 0, 0, 0.1);
-  //      `;
-
-  //       test1.style.cssText = `
-  //       position:fixed;
-  //       left: 50%;
-  //       transform: translateX( -50% );
-  //       width:1050px;
-  //        top:0;
-
-  //       `;
-  //     } else {
-  //       deliveryBtn.current.style = `display:block`;
-  //       mainIcons.current.style = `display:none`;
-  //       searchForm.current.style = `display:none`;
-  //       testBorder.current.style = `;
-  //     width:100%;
-  //     `;
-  //       test1.style.cssText = `position:relative;
-  //     width:1050px;
-  //       `;
-  //     }
-  //   });
-  // }, []);
-  //------------------------------------------------------------
-
-  // useEffect(() => {
-
-  //   //getElementById를 이용하여 특정elem를 target하는 방법도 사용해보고 싶었다.
-  //   //이 외의 나머지 대부분 방식은 useRef를 사용했다.
-  //   const test1 = document.getElementById('1');
-  //   const listener = () => {
-  //     const isAllElementFound =
-  //       deliveryBtn.current &&
-  //       mainIcons.current &&
-  //       searchForm.current &&
-  //       testBorder.current;
-
-  //     if (window.scrollY > 190 && isAllElementFound) {
-  //       deliveryBtn.current.style = `display:none`;
-  //       mainIcons.current.style = `display:block`;
-  //       searchForm.current.style = `display:block`;
-
-  //       testBorder.current.style = `position:fixed;
-  //       left: 50%;
-  //       width:100%;
-  //       height:66.39px;
-  //       top:0;
-  //       transform: translateX(-50%);
-  //       box-shadow: 0rem 0.25rem 1.5rem rgba(0, 0, 0, 0.1);
-  //      `;
-
-  //       test1.style.cssText = `
-  //       position:fixed;
-  //       left: 50%;
-  //       transform: translateX( -50% );
-  //       width:1050px;
-  //        top:0;
-
-  //       `;
-  //     } else {
-  //       deliveryBtn.current.style = `display:block`;
-  //       mainIcons.current.style = `display:none`;
-  //       searchForm.current.style = `display:none`;
-  //       testBorder.current.style = `;
-  //     width:100%;
-  //     `;
-  //       test1.style.cssText = `position:relative;
-  //     width:1050px;
-  //       `;
-  //     }
-  //   };
-
-  //   window.addEventListener('scroll', listener);
-
-  //   return () => {
-  //     window.removeEventListener('scroll', listener);
-  //   };
-  // }, []);
 
   return (
     <nav className={styles.headerNav}>
