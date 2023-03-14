a7296093170b728a7c5b46ac493dfe3d0120636f
PracticeKJY <poiijtl1004@naver.com>
2023-03-15 02:11:32 +0900

    ✨ [feat]  검색 창 기능 구현 #76
    SearchForm,SearchInput에 커스텀 훅을 적용하여 검색창 기능을 구현 하였습니다.

--- src/components/Header/SearchInput/SearchInput.jsx
+++ src/components/Header/SearchInput/SearchInput.jsx
@@ -1,19 +1,18 @@
 import styles from './SearchInput.module.css';
-import { atom, useRecoilState } from 'recoil';
 import Search from '@/assets/header/ic-search.svg';
 import { inputState } from '../@recoil/inputState';
+import { inputHook } from '../@recoilhooks/inputHook';
 
 // console.log(atom);
 
 console.log(inputState);
 
 function SearchInput() {
-  const [input, setInput] = useRecoilState(inputState);
+  const { input, setInputValue, setSubmitValue, removeValue } = inputHook();
 
   return (
     <>
       <h2 className={styles.a11yHidden}>검색</h2>
-      {/* <form action="#" className={styles.searchForm} method="post"> */}
       <div className={styles.searchFormInner}>
         <div className={styles.formGroup}>
           <form action="#" method="post">
@@ -24,11 +23,20 @@ function SearchInput() {
                 placeholder="검색어를 입력해주세요"
                 required={true}
                 type="text"
+                value={input}
+                onChange={setInputValue}
               />
             </label>
           </form>
         </div>
-        <button className={styles.formGroupButton} type="submit">
+        <button
+          onClick={() => {
+            setSubmitValue();
+            removeValue();
+          }}
+          className={styles.formGroupButton}
+          type="button"
+        >
           <img alt="검색하기" src={Search} />
         </button>
       </div>
