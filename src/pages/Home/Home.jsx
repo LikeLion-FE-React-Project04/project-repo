7e5325dac0c3ae91bdf98e33a0780d149865ff20
SeoMiYoung <seo20seo00@naver.com>
2023-03-14 18:10:26 +0900

    ✨ [feat] 쿠키 관련 이슈 질문을 위한 중간 커밋 #11

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,17 +1,51 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import { Footer } from '../../components/Footer/Footer';
 
 import { Mainmodal } from './Mainmodal/Mainmodal';
 
 function Home() {
-  const [isModalOpen, setModalOpen] = useState(true);
+  // 창 로드 -> 쿠키 있는지 확인
+  //               if)쿠키있음: 사용자가 오늘하루안보기 버튼을 누른적이 있다
+  //               if)쿠키없음: 사용자가 오늘하루안보기 버튼을 누른적다
+
+  const [isModalOpen, setModalOpen] = useState(true); // 초기 상태는 true로 설정
+  
+  useEffect(()=>{
+    // 특정 name의 쿠키가 존재하는지 확인하는 함수
+    function getCookie(name) {
+      console.log("getCookie실행");
+      const value = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)'); // 아마 name과 매치하는 쿠키가 있는지 확인하는듯
+      // console.log(value[2]); 안찍혀요 ㅠㅠ
+
+      if (value) {
+        return value[2]; // 두번째 인덱스로 T반환
+      }
+      else 
+        return null; // 쿠키가 존재하지 않으면 null반환
+    };
+
+    console.log("쿠키가 있는지 없는지 확인중~");
+    // 문서가 로드되면 쿠키가 있는지 없는지 확인함
+    window.addEventListener('DOMContentLoaded', function () {
+      //console.log(`화면 다시 로딩 isModalOpen값=>${isModalOpen}`);
+      if (getCookie('modalClose')) { // 'modalClose'라는 이름의 쿠키가 존재한다면 -> 다시 모달창을 띄울 필요가 없음
+        setModalOpen(false); // 모달창 숨기기
+      }
+      else { // 'modalClose'라는 이름의 쿠키가 없다면 -> 모달창 띄워야 함
+        setModalOpen(true); // 모달창 띄우기
+      }
+    });
+    console.log("확인끝~");
+    console.log(`렌더링/현재상태:${isModalOpen}`);
+  });
 
   return (
     <>
       <div>Home</div>
       <Footer/>
-      {isModalOpen && <Mainmodal setModalOpen={setModalOpen} />}
+      {/* 모달쿠키가 없으면 모달을 보이게 한다 */}
+      {isModalOpen ? <Mainmodal setModalOpen={setModalOpen} /> : null}
     </>
   );
 }
--- src/pages/Home/Mainmodal/Mainmodal.jsx
+++ src/pages/Home/Mainmodal/Mainmodal.jsx
@@ -7,6 +7,16 @@ export function Mainmodal({setModalOpen}) {
   const closeModal = () => {
     setModalOpen(false);
   };
+  
+  const closeModalDuringToday = () => {
+      // 하루 후의 데이터 값 가져오기
+      let date = new Date(Date.now() + 86400e3);
+
+      date = date.toUTCString();
+      document.cookie = `modalClose=T; expires=${date}`; // 하루뒤에 만료되는 쿠키 생성
+      
+      closeModal(); // 창 닫기
+  }
 
   return (
     <>
@@ -14,7 +24,7 @@ export function Mainmodal({setModalOpen}) {
         <div className={styles.popUpContainer}>
           <div>
             <Link className={styles.popUpBeauty} to="/"></Link>
-          <button type="button">오늘 하루 안 보기</button>
+          <button type="button" onClick={closeModalDuringToday}>오늘 하루 안 보기</button>
           <button className={styles.modalCloseBtn} type="button" onClick={closeModal}>닫기</button>
           </div>
         </div>
