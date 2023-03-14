1a8be5b2da687607a6a3b641b1117da6d851b099
PracticeKJY <poiijtl1004@naver.com>
2023-03-14 17:55:30 +0900

    ✨ [feat] Nav 컴포넌트화 및 관심사의 분리, 스크롤 이벤트 기능 구현 #70

--- src/components/Header/HeaderNav/CategoryButton.jsx
+++ src/components/Header/HeaderNav/CategoryButton.jsx
@@ -6,50 +6,55 @@ import { categoryData } from './categoryDate';
 
 import Hamburger from '@/assets/icons/Icon/header-hamburger.svg';
 
-function CategoryData({ href, title, img, alt }) {
+const CategoryData = ({ href, title, img, alt }) => {
   return (
-    <li>
+    <li className={styles.test01}>
       <a href={href}>
         <img src={img} alt={alt} />
         {title}
       </a>
     </li>
   );
-}
+};
 
-export const CategoryButton = () => {
+const CategoryButton = () => {
   const [isHover, setIsHover] = useState(false);
 
   return (
-    <li className={styles.headerNavProductList}>
-      <button
-        aria-label="카테고리 열기"
-        className={styles.headerNavDropdown}
-        type="button"
-        onMouseOver={() => setIsHover(true)}
-        onMouseOut={() => setIsHover(false)}
-        onFocus={() => setIsHover(true)}
-        onBlur={() => setIsHover(false)}
-      >
-        <img
-          aria-label="메뉴 아이콘"
-          className={styles.headerNavHamburger}
-          src={Hamburger}
-        />
-        카테고리
-        <ul className={styles.NavSubMenu}>
-          {categoryData.map(
-            (categoryData) =>
-              isHover && (
-                <CategoryData
-                  key={categoryData.id}
-                  title={categoryData.title}
-                  img={categoryData.imgSrc}
-                />
-              )
-          )}
-        </ul>
-      </button>
-    </li>
+    <ul>
+      <li className={styles.headerNavProductList}>
+        <button
+          aria-label="카테고리 열기"
+          className={styles.headerNavDropdown}
+          type="button"
+          onMouseOver={() => setIsHover(true)}
+          onMouseOut={() => setIsHover(false)}
+          onFocus={() => setIsHover(true)}
+          onBlur={() => setIsHover(false)}
+        >
+          <img
+            aria-label="메뉴 아이콘"
+            className={styles.headerNavHamburger}
+            src={Hamburger}
+          />
+          카테고리
+          <ul className={styles.NavSubMenu}>
+            {categoryData.map(
+              (categoryData) =>
+                isHover && (
+                  <CategoryData
+                    key={categoryData.id}
+                    title={categoryData.title}
+                    img={categoryData.imgSrc}
+                    href={categoryData.href}
+                  />
+                )
+            )}
+          </ul>
+        </button>
+      </li>
+    </ul>
   );
 };
+
+export default CategoryButton;
