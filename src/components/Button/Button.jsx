db946d308c5467c7fae05ae176fb0f0f13cc3788
= <=>
2023-03-14 15:13:20 +0900

    ✨[FEATURE] 버튼 className 커스텀 기능 추가 #15

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,15 +1,20 @@
-import styles from "./Button.module.scss";
+import styles from './Button.module.scss';
 
 export function Button({
   uiType = 'primary',
   children,
+  className = '',
   ...restProps
 }) {
-  const combinedClassNames = `${styles.button} ${getUiStyle(uiType)}`
+  const combinedClassNames = `${styles.button} ${getUiStyle(
+    uiType
+  )} ${className}`;
 
   return (
-    <button className={combinedClassNames} {...restProps}>{children}</button>
-  )
+    <button className={combinedClassNames} {...restProps}>
+      {children}
+    </button>
+  );
 }
 
 function getUiStyle(uiType) {
