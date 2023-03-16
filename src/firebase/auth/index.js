ee785f4a683aa490537a129b6a09f1f09f645e94
seo seok won <skowko6750@hansung.ac.kr>
2023-03-16 11:46:42 +0900

    ✨ [feat] 회원가입 로직 구현 #2 (#97)
    Co-authored-by: = <=>

--- src/firebase/auth/index.js
+++ src/firebase/auth/index.js
@@ -5,7 +5,7 @@ import { app } from '../app';
 export const auth = getAuth(app);
 auth.useDeviceLanguage();
 
-// export * from './useSignUp';
+export * from './useSignUp';
 export * from './useSignIn';
 export * from './useSignOut';
 export * from './useAuthState';
---
+++ src/firebase/firestrore/index.js
@@ -0,0 +1,6 @@
+import { app } from '../app';
+import { getFirestore } from 'firebase/firestore';
+
+export const db = getFirestore(app);
+
+export * from './useCreateAuthUser';
