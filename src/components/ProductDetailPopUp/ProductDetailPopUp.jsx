bc8a5f5a68935594c8abbb87a14c9caa4b7c811e
SeoMiYoung <seo20seo00@naver.com>
2023-03-20 13:32:02 +0900

    ✨ [feat] textarea에 글을 입력함에 따라서 placeholder와 글자수 조절하기 구현 #96

--- src/components/ProductDetailPopUp/ProductDetailPopUp.jsx
+++ src/components/ProductDetailPopUp/ProductDetailPopUp.jsx
@@ -7,23 +7,37 @@ import { PlaceholderReview } from './PlaceholderReview/PlaceholderReview';
 import { default as PageTitle } from '@/components/PageTitle/PageTitle.jsx'
 import productImg from "@/assets/product/tangtang/thumbnail.jpg";
 
-
-
 export function ProductDetailPopUp({uiType}) {
+  // textarea를 참조하기 위한 ref
   const area = useRef(); 
+  // textarea의 text를 관리하기 위한 ref
+  const textAreaText = useRef('');
 
-  // Placeholder가 띄워지냐 안지냐를 관리하는 state
+  // placeholder가 띄워야할지 말지를 관리하는 state
   const [isActiveP, setIsActiveP] = useState(true); 
-  
+  // textarea의 글자수를 관리하는 state
+  const [inputCount, setInputCount] = useState(0);
+
+  // div태그(.textAreaWrap)을 클릭하면, 발생하는 이벤트
   function handlePlaceholder() { 
-    setIsActiveP(false); // 상위 div태그(.textAreaWrap)을 클릭하면 placeholder는 사라져야 함
-    area.current.focus(); // 동시에, textarea에 focus를 줘야함
+    setIsActiveP(false); // placeholder는 사라져야 함. Why? 입력을 받아야되니..
+    area.current.focus(); // 동시에, textarea에 focus를 줘야함. Why? 입력을 해야되니..
   }
 
-  function handlePlaceholderT() { // textArea의 focus가 해지될때 실행
-    // useRef로 글자수를 관리하되, 글자수가 0일때만 다음 함수를 실행시키록
-    setIsActiveP(true); // 다시 placeholder를 띄움
+  // textarea의 focus가 해제될 때 발생하는 이벤트
+  function handlePlaceholderT() { 
+    if(inputCount == 0) { // 사용자가 아무것도 입력하지 않았을때만
+      setIsActiveP(true); // 다시 placeholder를 띄움
+    }
   }
+
+  // textarea의 내용에 변화가 있을 때 발생하는 이벤트
+  const textareaInputHandler = (e) => {
+    // textarea의 내용이 바뀔때마다 ref의 current값을 변경해줌 => but리랜더링은 안되므로 화면에 반영이 안됨
+    textAreaText.current = e.target.value;
+    // 그래서 랜더링을 시키기 위해서 state 사용
+    setInputCount(textAreaText.current.length);
+  };
 
   return (
     <>
@@ -44,9 +58,9 @@ export function ProductDetailPopUp({uiType}) {
           <fieldset className={styles.inquiryContentWrapper}>
             <label htmlFor='inquiryText'>내용</label>
             <div className={styles.textAreaWrap} onClick={handlePlaceholder}>
-              <textarea id="inquiryText" inputMode='text' name="content" ref={area} required maxLength="5000" onBlur={handlePlaceholderT}></textarea>
+              <textarea onChange={textareaInputHandler} id="inquiryText" inputMode='text' name="content" ref={area} required maxLength="5000" onBlur={handlePlaceholderT}></textarea>
               {isActiveP?(uiType=='inquiry'?<PlaceholderInquiry />:<PlaceholderReview />):null}
-              <p className={styles.inquiryCount} id="inquiryCount">0/5000</p>
+              <p className={styles.inquiryCount} id="inquiryCount">{inputCount}/5000</p>
             </div>
           </fieldset>
         </div>
