37035b00cc8f3cb833869f1c04cd369adfddb97f
= <=>
2023-03-21 14:49:37 +0900

    ✅[TEST] CartModal 컴포넌트 portal 로직 적용 #126

--- src/components/CartModal/CartModalLayout.jsx
+++ src/components/CartModal/CartModalLayout.jsx
@@ -2,15 +2,17 @@ import CartModal from './CartModal';
 import styles from './CartModalLayout.module.scss';
 import { productCartModalState } from '@/store/cartModalState.js';
 import { useRecoilValue } from 'recoil';
+import ReactDOM from 'react-dom';
 
 function CartModalLayout() {
   const isVisible = useRecoilValue(productCartModalState);
 
   if (isVisible) {
-    return (
+    return ReactDOM.createPortal(
       <div className={styles.cartModalLayout}>
         <CartModal />
-      </div>
+      </div>,
+      document.getElementById('cartModalPortal')
     );
   }
 }
