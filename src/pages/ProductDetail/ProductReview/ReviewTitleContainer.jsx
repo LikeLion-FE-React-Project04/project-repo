697e7138a73113c0313b77bcec24365d65ad8851
tkdkfkgk <mun961@gmail.com>
2023-03-22 00:35:43 +0900

    ✨ [feat] 상품후기 모달 팝업창 연결 #98

--- src/pages/ProductDetail/ProductReview/ReviewTitleContainer.jsx
+++ src/pages/ProductDetail/ProductReview/ReviewTitleContainer.jsx
@@ -1,8 +1,21 @@
+import { useSetRecoilState } from 'recoil';
+
 import styles from "./ReviewTitleContainer.module.scss";
 
 import { Button, PageTitle } from '@/components';
+import { productDetailModalState } from '@/store/detailModalState.js';
+import { darkFilterState } from '@/store/darkFilterState.js';
+
 
 export default function ReviewTitleContainer() {
+  const setProductDetailModalState = useSetRecoilState(productDetailModalState);
+  const setDarkFilterState = useSetRecoilState(darkFilterState);
+
+  function productModalClickHandler() {
+    setProductDetailModalState(true);
+    setDarkFilterState(true);
+  }
+
   return (
     <div className={styles.productReviewFrame}>
       <div>
@@ -14,7 +27,7 @@ export default function ReviewTitleContainer() {
           <li className={styles.li}> 작성하신 후기는 확인 후 적립금이 지급됩니다. (영업일 기준 평균 1~2일 소요)</li>
         </ul>
       </div>
-      <Button type='button' uiType='fifth'>후기 작성하기</Button>
+      <Button uiType='fifth' onClick={productModalClickHandler}>후기 작성하기</Button>
     </div>
   )
 }
