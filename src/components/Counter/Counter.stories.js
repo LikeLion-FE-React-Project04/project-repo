799ee2741ec71ee1d546ffdd096633a11cdd1867
= <=>
2023-03-21 10:05:02 +0900

    ✅ [TEST] Counter 스토리북 구현 #126

---
+++ src/components/Counter/Counter.stories.js
@@ -0,0 +1,10 @@
+import Counter from './Counter.jsx';
+
+export default {
+  title: 'components/Counter',
+  component: Counter,
+  tags: ['autodocs'],
+  args: { ...Counter.defaultProps },
+};
+
+export const Primary = {};
