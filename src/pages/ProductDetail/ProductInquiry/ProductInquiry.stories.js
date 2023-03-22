88a3272f5c27dc0217b4f5f3cb112c0dc63a116e
SeoMiYoung <seo20seo00@naver.com>
2023-03-22 17:22:37 +0900

    ✨ [feat] 아코디언 컴포넌트를 사용할 수 있게 마크업 및 CSS 하기 (질문을 위한 중간커밋) #96

---
+++ src/pages/ProductDetail/ProductInquiry/ProductInquiry.stories.js
@@ -0,0 +1,11 @@
+import ProductInquiry from './ProductInquiry.jsx';
+
+export default {
+  title: 'components/ProductInquiry',
+  component: ProductInquiry,
+  tags: ['autodocs'],
+  args: { ...ProductInquiry.defaultProps },
+};
+
+export const ProductInquiryPart = {};
+
