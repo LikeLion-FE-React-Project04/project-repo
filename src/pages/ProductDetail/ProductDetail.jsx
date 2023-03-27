179ba3c41e99e8b65b2ca9381cb7761537f9c64b
= <=>
2023-03-27 16:37:51 +0900

    ✨ [FEATURE] ProductThumbnail 컴포넌트 페이지에 적용 #156

--- src/pages/ProductDetail/ProductDetail.jsx
+++ src/pages/ProductDetail/ProductDetail.jsx
@@ -1,4 +1,5 @@
 import { useLoaderData, useParams, Link } from 'react-router-dom';
+import { useEffect } from 'react';
 import { useRecoilValue } from 'recoil';
 
 import { ProductDetailPopUp } from '../../components/ProductDetailPopUp/ProductDetailPopUp';
@@ -9,17 +10,18 @@ import ProductInquiry from './Productinquiry/Productinquiry';
 import ProductReview from './ProductReview/ProductReview';
 
 import { productListFamily } from '@/store/productListState.js';
+import ProductThumbnail from './ProductThumbnail/ProductThumbnail';
 
 function ProductDetail() {
   const { productId } = useParams();
   const product = useRecoilValue(productListFamily(productId));
 
-  console.log(product);
 
   return (
     <div className={styles.ProductDetailWrapper}>
       <h2>ProductDetail {productId}</h2>
       <Link to="/">Go to Home</Link>
+      <ProductThumbnail product={product} />
       <ProductReview />
       <ProductInquiry />
       <ProductDetailPopUpLayout />
