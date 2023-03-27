cb8c8216a4255529e67a5128827a2c8fe06e1be8
= <=>
2023-03-27 10:42:06 +0900

    ✨ Cart 페이지 컴포넌트 구현 #150

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,10 +1,29 @@
 // import AccordionItem from '@/components/Accordian/AccordionItem.jsx'
+import PageTitle from '../../components/PageTitle/PageTitle';
+import Address from './Address/Address';
+import styles from './Cart.module.scss';
+import { Button } from '@/components/Button/Button';
+
+import CartAccordion from './CartAccordion/CartAccordion';
+import PriceInfo from './PriceInfo/PriceInfo';
 
 function Cart() {
   return (
-    <div>
-      Cart
-      {/* <AccordionItem /> */}
+    <div className={styles.cart}>
+      <div className={styles.title}>
+        <PageTitle>장바구니</PageTitle>
+      </div>
+
+      <div style={{ display: 'flex', flexDirection: 'row' }}>
+        <div style={{ marginRight: '24px' }}>
+          <CartAccordion />
+        </div>
+        <div>
+          <Address />
+          <PriceInfo></PriceInfo>
+          <Button className={styles.orderBtn}>주문하기</Button>
+        </div>
+      </div>
     </div>
   );
 }
