e708abcc4c1d8c72cf7a06226b8a948cd978f696
kelly121212 <69451362+kelly121212@users.noreply.github.com>
2023-03-27 14:52:21 +0900

    ✨ [Feat] 제품 상세페이지 상단 컴포넌트 완성 #122 (#153)

---
+++ src/pages/ProductDetail/ProductThumbnail/ProductThumbnail.stories.js
@@ -0,0 +1,11 @@
+import ProductThumbnail from './ProductThumbnail.jsx';
+// import style from '../../styles/main.module.css';
+
+export default {
+  title: 'components/ProductThumbnail',
+  component: ProductThumbnail,
+  tags: ['autodocs'],
+  args: { ...ProductThumbnail.defaultProps },
+};
+
+export const Primary = {};
