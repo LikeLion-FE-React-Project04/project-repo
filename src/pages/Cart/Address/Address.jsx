9ec24f176148a641923eafa83d7420421badcb7c
seo seok won <skowko6750@hansung.ac.kr>
2023-03-28 13:23:21 +0900

    Feat/#156 기능 추가 및 버그 수정 (#177)
    * 🎨 [REFACTORIGN ]ProductThumbnail 로직 수정
    
    * 🐛 [BUG] 세그먼트 라우팅 버그 수정
    
    * ⚡️ [REFACTORIGN] ProductCard 컴포넌트에 리스트페이지에 맞게 수정 #170
    
    * ✨ [FEATURE] 결제 버튼 로직 추가  #170
    
    * 🐛 [BUG] 캐로셀 버그 수정 및 간단한 수정 #170
    
    ---------
    
    Co-authored-by: = <=>

--- src/pages/Cart/Address/Address.jsx
+++ src/pages/Cart/Address/Address.jsx
@@ -1,21 +1,54 @@
 import styles from './Address.module.scss';
 import { Button } from '@/components/Button/Button';
 import location from '@/assets/header/ic-location.svg';
+import DaumPostcode from 'react-daum-postcode';
+import { useState } from 'react';
+import { addressState, addressNumberState } from '@/store/addressState.js';
+import { useRecoilState } from 'recoil';
 
 function Address() {
+  const [openPostcode, setOpenPostcode] = useState(false);
+  const [address, setAddress] = useRecoilState(addressState);
+  const [addressNumber, setAddressNumber] = useRecoilState(addressNumberState);
+
+  const handle = {
+    // // 버튼 클릭 이벤트
+    clickButton: () => {
+      setOpenPostcode((current) => !current);
+    },
+    // 주소 선택 이벤트
+    selectAddress: (data) => {
+      setAddress(data.address);
+      setAddressNumber(data.zonecode);
+      setOpenPostcode(false);
+    },
+  };
+
   return (
     <div className={styles.address}>
       <div className={styles.title}>
         <img src={location} alt="배송지" width={24} height={24} />
         배송지
       </div>
-      <div className={styles.info}>
-        서울 중량구 면목로 42길 11 (행운빌딩) 603호
-      </div>
+      <div className={styles.info}>{address}</div>
       <div className={styles.mark}>샛별배송</div>
-      <Button uiType="third" className={styles.addressButton}>
+
+      <Button
+        uiType="third"
+        className={styles.addressButton}
+        onClick={handle.clickButton}
+      >
         주소 검색
       </Button>
+      {openPostcode && (
+        <div className={styles.addressModal}>
+          <DaumPostcode
+            onComplete={handle.selectAddress} // 값을 선택할 경우 실행되는 이벤트
+            autoClose={false} // 값을 선택할 경우 사용되는 DOM을 제거하여 자동 닫힘 설정
+            defaultQuery="판교역로 235" // 팝업을 열때 기본적으로 입력되는 검색어
+          />
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/pages/Cart/PaymentPrice/PaymentPrice.jsx
@@ -0,0 +1,60 @@
+import { Button } from '@/components/Button/Button';
+import styles from './PaymentPrice.module.scss';
+import { useAuthState } from '@/firebase/auth';
+import { useNavigate } from 'react-router-dom';
+import { addressState } from '@/store/addressState.js';
+import { useRecoilValue } from 'recoil';
+import { cartProductsState } from '@/store/cartModalState.js';
+import { countState } from '../../../store/CounterState';
+import { cartPriceData } from '@/store/cartModalState.js';
+import { getPriceFormat } from '@/utils';
+
+function PaymentPrice() {
+  const { user } = useAuthState();
+  const address = useRecoilValue(addressState);
+  const cartProducts = useRecoilValue(cartProductsState);
+  const count = useRecoilValue(countState);
+  const movePage = useNavigate();
+  const [totalPrice, totalSalePrice, totalPaymentPrice] =
+    useRecoilValue(cartPriceData);
+
+  let cartsInfo = '';
+
+  cartProducts.map((product) => {
+    cartsInfo += `${product.name} - ${count[product.id]}개\n`;
+  });
+
+  if (user) {
+    return (
+      <Button
+        className={styles.orderBtn}
+        onClick={() => {
+          alert(
+            `주문이 완료되었습니다\n\n이름: ${
+              user.displayName
+            }님\n배송지: ${address}\n\n -주문내역-\n${cartsInfo}\n총 결제금액: ${getPriceFormat(
+              totalPaymentPrice + 3000
+            )}원`
+          );
+        }}
+      >
+        주문하기
+      </Button>
+    );
+  } else {
+    return (
+      <Button
+        uiType={'third'}
+        className={styles.orderBtn}
+        onClick={() => {
+          alert('로그인이 필요합니다.');
+          movePage('/signin');
+        }}
+      >
+        로그인이 해주세요.
+      </Button>
+    );
+  }
+}
+
+export default PaymentPrice;
