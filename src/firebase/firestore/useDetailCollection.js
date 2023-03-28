4a3314c68c37ed0deaf3a00710a256895848fc04
SeoMiYoung <seo20seo00@naver.com>
2023-03-28 13:07:35 +0900

    🎨 [modify] index색인이 안먹어서 대체로 프론트엔드 개발 단에서 내림차순 정렬하느라 코드 수정 #96

--- src/firebase/firestore/useDetailCollection.js
+++ src/firebase/firestore/useDetailCollection.js
@@ -17,33 +17,41 @@ export const useDetailCollection = (collectionName) => {
   const [productIdState, setProductIdState] = useRecoilState(productIdAtom);
   
   
-  // snapshot에는 컬렉션이 들어있다
-  // snapshot은 문서(데이터)들을 배열로 저장한다 => [ {..}, {..}, {..} ]
+  // 상품 ID가 변경되면 productIdState atom을 업데이트 해야한다
   useEffect(() => {
-    // 최신 날짜순 정렬
     setProductIdState(product.id);
-    console.log("파이어스토어: ", productIdState);
-    const sortCollectionRecentDate = query(collection(db, collectionName), where("productId", "==", productIdState), orderBy("date", "desc"));
+  }, [product.id]);
 
-    onSnapshot(sortCollectionRecentDate, (snapshot) => {
-      let dataArr = [];
-  
-      // snapshot.docs는 배열이므로 forEach 사용 가능
-      snapshot.docs.forEach((doc) => {
-        dataArr.push({...doc.data()});
-      })
-      console.log(dataArr);
-
-      //recentDataCurrent = dataArr;
-
-      setDataState(dataArr);
-      //recentData.current = dataArr;
-      //recentDataCurrent = recentData.current;
-      console.log("현재 배열 상태", dataState);
-
-      //setDataState(dataArr);
-    });
-  }, []);
+  useEffect(() => {
+    if(productIdState) {
+      const sortCollectionRecentDate = query(collection(db, collectionName), where('productId', '==', productIdState));
+        // 원래 orderBy로 함께 쓰려고 했는데, Firestore의 색인(index) 생성 문제로 잘 작동이 안됨 ㅠㅠ
+        // 원래대로라면, orderBy('date', 'desc')를 넣어주고, 색인 설정하면 됨
+        // snapshot에는 컬렉션이 들어있다, snapshot은 문서(데이터)들을 배열로 저장한다 => [ {..}, {..}, {..} ]
+      const unsubscribe = onSnapshot(sortCollectionRecentDate, (snapshot) => {
+        let dataArr = [];
+      
+        // snapshot.docs는 배열이므로 forEach 사용 가능
+        snapshot.docs.forEach((doc) => {
+          dataArr.push({...doc.data()});
+        });
+        console.log("현재 배열 상태 dataArr로 찍어보기: ", dataArr);
+    
+        // Index를 사용 못하는 대체방안 => 현재 orderBy를 못했으니, 그걸 Front-end 개발 단에서 내림차순으로 정렬 처리해야 함
+        dataArr.sort(({date: aDate}, {date:bDate}) => {
+          // if) 오름차순 정렬 => return aDate.toDate() - bDate.toDate();
+          // if) 내림차순 정렬 => 최신순으로(우리가 하고 싶은 방향임)
+          return bDate.toDate() - aDate.toDate();
+        });
+    
+        setDataState(dataArr);
+        console.log("현재 배열 상태", dataState);
+      });
+    
+      return unsubscribe; // 스냅샷이 계속 작동되는걸 막음
+    }
+    
+  }, [productIdState]);
 
   return { dataState };
-}
+};
