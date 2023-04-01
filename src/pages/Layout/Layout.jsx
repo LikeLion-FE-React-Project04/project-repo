161351f84dc88a3f2b5b6c7afad0d51e4ea510fe
SeoMiYoung <seo20seo00@naver.com>
2023-04-01 09:58:18 +0900

    🐛 [feat] 스크롤 이동 문제 해결을 위한 컴포넌트 추가 #189

--- src/pages/Layout/Layout.jsx
+++ src/pages/Layout/Layout.jsx
@@ -3,10 +3,12 @@ import { NavLink, Outlet } from 'react-router-dom';
 import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
 import DarkFilter from '@/components/DarkFilter/DarkFilter';
+import ScrollToTop from '../../components/ScrollToTop/ScrollToTop';
 
 export default function Layout({ children }) {
   return (
     <>
+      <ScrollToTop />
       <DarkFilter />
       <Header />
       <main>
