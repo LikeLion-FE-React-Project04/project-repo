6733b700abd6229fb35ca3154c8e5dff31c5c679
SeoMiYoung <seo20seo00@naver.com>
2023-04-01 20:41:19 +0900

    🎨 [modify] 메인모달을 recoil+transparentFilter를 사용함으로써 발생한 변경사항들 #192

--- src/pages/Home/Mainmodal/useGetCookieEvent.js
+++ src/pages/Home/Mainmodal/useGetCookieEvent.js
@@ -1,21 +1,25 @@
 import { useState, useLayoutEffect } from 'react';
 
 import { getCookie } from '../../../utils';
+import { useRecoilState } from 'recoil';
+import { productMainmodalState } from './@recoil/MainmodalState';
 
 export const useGetCookieEvent = () => {
-  const [isModalOpen, setModalOpen] = useState(true);
+  // const [isModalOpen, setModalOpen] = useState(true);
+  // recoil로 바꾸기
+  const [isVisible, setIsVisible] = useRecoilState(productMainmodalState);
 
   useLayoutEffect(() => {
     let modalCloseValue = getCookie('modalClose');
 
     if (modalCloseValue && modalCloseValue?.includes('T')) { // 쿠키가 존재할 때
-      setModalOpen(false); // 모달창을 띄우지 않는다
+      setIsVisible(false); // 모달창을 띄우지 않는다
     }
   }, []); // 첫 랜더링시에만 동작
 
   return {
-    isModalOpen,
-    setModalOpen,
+    isVisible,
+    setIsVisible,
   };
 };
 
