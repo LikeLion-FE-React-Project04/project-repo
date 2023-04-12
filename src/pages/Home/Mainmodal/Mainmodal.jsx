600465c59d210e1468643b48fe57dbe04ce599ab
SeoMiYoung <seo20seo00@naver.com>
2023-04-12 14:35:10 +0900

    🎨 [modify] 메인모달창 불필요한 주석 정리 + 디테일한 부분 수정 #212

--- src/pages/Home/Mainmodal/Mainmodal.jsx
+++ src/pages/Home/Mainmodal/Mainmodal.jsx
@@ -22,8 +22,6 @@ export default function Mainmodal() {
   const modalRef = useRef();
 
   useEffect(() => {
-    // closeBtnRef.current.focus();
-    // 닫기 버튼에 주는게 아니라, 모달창 자체에 focus를 줌
     modalRef.current.focus();
     setTransparentFilterFocusState(modalRef.current); // 모달창 DOM자체를 저장해줌
   }, []);
@@ -62,7 +60,7 @@ export default function Mainmodal() {
     if(!isTabPressed) { // Tab키를 누르지 않은 경우
       console.log('Tab이 아닙니다.');
 
-      return; // 아무 반응을 하지 않고 끝낸다
+      return; 
     }
     if(isShiftPressed) { // Shift+Tab 누른 경우
       // document.activeElement는 현재 focus요소를 가리킨다
@@ -72,8 +70,6 @@ export default function Mainmodal() {
       }
     }
     else { // Shift를 누르지 않고, tab키만 눌렀을 경우
-      //console.log('lastFocusableElement: ', lastFocusableElement);
-      //console.log('document.activeElement: ', document.activeElement);
       if(document.activeElement === lastFocusableElement) {
         firstFocusableElement.focus(); // 다시 첫번째 요소로 focus
         e.preventDefault();
@@ -88,7 +84,7 @@ export default function Mainmodal() {
         aria-modal="true"
         className={styles.mainModalWrap}
         role="dialog"
-        aria-label="뷰티컬리에 대한 메인 모달창이 열렸습니다."
+        aria-label="뷰티칼리에 대한 메인 모달창이 열렸습니다."
         onKeyDown={handleModalKeyEvent}
         ref={modalRef}
         tabIndex="-1"
@@ -99,7 +95,7 @@ export default function Mainmodal() {
               className={styles.popUpBeauty} 
               to="/" 
               ref={moveToBeautyKarlyRef}
-              aria-label="뷰티컬리로 이동하기"
+              aria-label="뷰티칼리로 이동하기"
             />
             <button  
               type="button" 
