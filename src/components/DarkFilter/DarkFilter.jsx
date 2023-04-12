1ed104be792f527c90fe78fd2f925580b03c0720
seo seok won <skowko6750@hansung.ac.kr>
2023-04-12 13:33:46 +0900

    [BUGFIX, ACCESSIBILITY] 모달 밖으로 포커싱이 나가지 않도록 코드 수정 (#224)
    * 🐛 [BUGFIX] 컨벤션에 맞게 파일이름 수정 #221
    
    * 🎨 [REFACTORING] ProductsCarousel 컴포넌트 코드 정리 및 aira-label 추가 #202
    
    * 🐛 [BUGFIX, ACCESSIBILITY] 모달 밖으로 포커싱이 나가지 않도록 코드 수정 #206
    
    ---------
    
    Co-authored-by: = <=>

--- src/components/DarkFilter/DarkFilter.jsx
+++ src/components/DarkFilter/DarkFilter.jsx
@@ -1,12 +1,22 @@
-import styles from './DarkFilter.module.scss'
+import styles from './DarkFilter.module.scss';
 
-import {darkFilterState} from '@/store/darkFilterState.js'
+import { darkFilterState } from '@/store/darkFilterState.js';
 import { useRecoilValue } from 'recoil';
+import { darkFilterFocusState } from '../../store/darkFilterState';
 
 function DarkFilter() {
   let isActive = useRecoilValue(darkFilterState);
+  const darkFilterFocus = useRecoilValue(darkFilterFocusState);
 
-  return isActive? <div className={styles.darkFilter} />: null;
+  const handleFilterClick = (e) => {
+    if (darkFilterFocus) {
+      darkFilterFocus.focus();
+    }
+  };
+
+  return isActive ? (
+    <div className={styles.darkFilter} onClick={handleFilterClick} />
+  ) : null;
 }
 
-export default DarkFilter
+export default DarkFilter;
--- src/store/darkFilterState.js
+++ src/store/darkFilterState.js
@@ -1,6 +1,11 @@
-import { atom, /* selector */ } from 'recoil';
+import { atom /* selector */ } from 'recoil';
 
 export const darkFilterState = atom({
   key: 'darkFilterState',
   default: false,
+});
+
+export const darkFilterFocusState = atom({
+  key: 'darkFilterFocusState',
+  default: null,
 });
