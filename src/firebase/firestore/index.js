696272511df27b8e1fc18148259237233fa673f5
PracticeKJY <poiijtl1004@naver.com>
2023-04-13 19:22:40 +0900

    ✨ [feat] productListState 데이터 일괄 업로드#232
    -firebase의 writeBatch()를 이용하여 데이터를 일괄 업로드하는 writeBatchData.js 를 만들었습니다.
    -header.jsx에 임시로 핸들러와 버튼을 달아주고 데이터를 업로드 시킨 뒤, 주석처리 하였습니다.

--- src/firebase/firestore/index.js
+++ src/firebase/firestore/index.js
@@ -4,3 +4,5 @@ import { getFirestore } from 'firebase/firestore';
 export const db = getFirestore(app);
 
 export * from './useCreateAuthUser';
+export * from './writeBatchData';
+export * from './useRead';
