054a49938fb097fa2761665e8a5949a979510a33
= <=>
2023-04-13 21:12:20 +0900

    ✨[FEATURE] BirthInput 컴포넌트 로직 설계 #215

---
+++ src/pages/SignUp/BirthInput/BirthInput.jsx
@@ -0,0 +1,66 @@
+import { useRef, useState } from 'react';
+import styles from './BirthInput.module.scss';
+import Input from '@/pages/SignUp/Input/Input';
+import { checkValidation } from '../../../utils';
+import { useRecoilState } from 'recoil';
+import { signUpFormState } from '../@recoil/signUp';
+
+function BirthInput() {
+  const [signUpForm, setSignUpForm] = useRecoilState(signUpFormState);
+  const [warnPragment, setWarnPragment] = useState('');
+
+  const handleChange = (e) => {
+    e.target.value = e.target.value
+      .replace(/[^0-9.]/g, '')
+      .replace(/(\..*)\./g, '$1');
+
+    const name = e.target.name;
+
+    let pragment;
+
+    setSignUpForm((prev) => {
+      const tmp = { ...prev };
+      const birthTmp = { ...tmp.birth };
+      birthTmp[name] = e.target.value;
+      pragment = checkValidation('birth', birthTmp);
+      tmp.birth = birthTmp;
+      return tmp;
+    });
+
+    setWarnPragment(pragment);
+  };
+  return (
+    <Input text={'생년월일'}>
+      <div className={styles.inputLayout}>
+        <div className={styles.BirthInput}>
+          <input
+            type="text"
+            name="year"
+            placeholder={'YYYY'}
+            maxLength={'4'}
+            onChange={handleChange}
+          />
+          <span>/</span>
+          <input
+            name="month"
+            type="text"
+            placeholder={'MM'}
+            maxLength={'2'}
+            onChange={handleChange}
+          />
+          <span>/</span>
+          <input
+            name="day"
+            type="text"
+            placeholder={'DD'}
+            maxLength={'2'}
+            onChange={handleChange}
+          />
+        </div>
+        <span className={styles.validationPhrases}>{warnPragment}</span>
+      </div>
+    </Input>
+  );
+}
+
+export default BirthInput;
