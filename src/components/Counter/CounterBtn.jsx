17e4b062ffd8195e892cad8afa446396c0acf5a2
SeoMiYoung <seo20seo00@naver.com>
2023-04-14 18:49:28 +0900

    ✨ [feat] 장바구니 페이지를 제외한 버튼은 작동 완료 #243

--- src/components/Counter/CounterBtn.jsx
+++ src/components/Counter/CounterBtn.jsx
@@ -1,13 +1,15 @@
 import classNames from 'classnames';
-import { useSetRecoilState } from 'recoil';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
 import { useEffect, useRef } from 'react';
 
 import styles from './CounterBtn.module.scss';
+import { countState } from '@/store/CounterState.js';
 
 import {
   countMinusBtnRefState,
   countPlusBtnRefState,
 } from '@/store/CounterState.js';
+import { productListFamily, selectedproductId } from '../../store/productListState';
 
 function CounterBtn({ type = 'plus', ...restProps }) {
   const setCountMinusBtnRef = useSetRecoilState(countMinusBtnRefState);
@@ -20,6 +22,10 @@ function CounterBtn({ type = 'plus', ...restProps }) {
     styleType = styles.minus;
     message = '수량 1 감소';
   }
+  
+  const count = useRecoilValue(countState);
+  const productId = useRecoilValue(selectedproductId);
+  const product = useRecoilValue(productListFamily(productId));
 
   useEffect(() => {
     if (type == 'minus') {
@@ -28,6 +34,26 @@ function CounterBtn({ type = 'plus', ...restProps }) {
       setCountPlusBtnRef(btnRef.current);
     }
   }, [btnRef]);
+
+  useEffect(()=>{
+    if(count[productId] == 1) { // 마이너스 버튼 비활성화 
+      if(type=="minus") {
+        btnRef.current.style.background = "url('assets/modal/ic-minus-disabled.svg') 50% 50% / 100%";
+      }
+      if(type=="plus") {
+        btnRef.current.style.background = "url('assets/modal/ic-plus-enabled.svg') 50% 50% / 100%";
+      }
+    }
+    else {
+      if(type=="minus") {
+        btnRef.current.style.background = "url('assets/modal/ic-minus-enabled.svg') 50% 50% / 100%";
+      }
+      if(type=="plus") {
+        btnRef.current.style.background = "url('assets/modal/ic-plus-enabled.svg') 50% 50% / 100%";
+      }
+    }
+  }, [count, productId]);
+
 
   return (
     <button
