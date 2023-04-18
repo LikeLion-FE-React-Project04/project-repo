13e86fa0efbe8623d50774592671d02e001403d0
seo seok won <skowko6750@hansung.ac.kr>
2023-04-18 23:22:05 +0900

    🎨 [MODIFY] alertbox 사용 조건에 맞게 코드 수정 (#252)
    * 🎨 [REFACTORING] 회원가입 경고 모달 커스텀 훅 적용 #220
    
    * 🎨 [MODIFY] alertbox 사용 조건에 맞게 코드 수정 #220
    
    ---------
    
    Co-authored-by: = <=>

--- src/pages/Cart/CartAccordion/CartAccordionPanelItem.jsx
+++ src/pages/Cart/CartAccordion/CartAccordionPanelItem.jsx
@@ -1,68 +1,30 @@
+import { useRecoilState } from 'recoil';
+
+import { cartDataState } from '../../../store/cartModalState';
+
 import styles from './CartAccordionPanelItem.module.scss';
-import testImg from '@/assets/product/tangtang/thumbnail.jpg';
+import CartDataCheckBox from './CartDataCheckBox';
+
 import Counter from '@/components/Counter/Counter.jsx';
 import { getPriceFormat } from '@/utils';
-import CartDataCheckBox from './CartDataCheckBox';
-import { useRecoilState } from 'recoil';
-import { countState } from '@/store/CounterState';
-import { cartDataState } from '../../../store/cartModalState';
-import {
-  alertModalMoveState,
-  alertModalState,
-  alertModalText,
-  alertModalUiType,
-  caseOfRemoveProduct,
-} from '../../../components/AlertBox/@recoil/alertModalState';
-import { useSetRecoilState } from 'recoil';
-import { darkFilterState } from '../../../store/darkFilterState';
-import { useRecoilValue } from 'recoil';
 
-function CartAccordionPanelItem({
-  product = {
-    id: 'product-ekfk',
-    name: '[풀무원] 탱탱쫄면 (4개입)',
-    description: '튀기지 않아 부드럽고 매콤한',
-    price: 4980,
-    saleRatio: 0,
-    salePrice: 0,
-    image: {
-      thumbnail: 'assets/product/tangtang/thumbnail.jpg',
-      view: 'tangtang/detail-view.jpg',
-      banner: 'tangtang/detail-banner.jpg',
-      info: 'tangtang/detail-info.jpg',
-      alt: '풀무원 탱탱쫄면',
-    },
-    stock: 11,
-    category: '샐러드ㆍ간편식',
-    kalryOnly: 'false',
-    brand: '풀무원',
-    storageType: 'frozen',
-  },
-}) {
+import { countState } from '@/store/CounterState';
+
+
+import { useAlertBox } from '@/components/AlertBox/@hook/useAlertBox.js';
+
+function CartAccordionPanelItem({ product = {} }) {
   const [cartData, setCartData] = useRecoilState(cartDataState);
   const [count, setCount] = useRecoilState(countState);
-
-  // 경고창 컴포넌트와 연결하려고 추가한 부분
-  const setDarkFilterState = useSetRecoilState(darkFilterState);
-  const setAlertModalState = useSetRecoilState(alertModalState);
-  const setAlertModalText = useSetRecoilState(alertModalText);
-  const setAlertModalMoveState = useSetRecoilState(alertModalMoveState);
-  const setAlertModalUiType = useSetRecoilState(alertModalUiType);
-  const setCaseOfRemoveProduct = useSetRecoilState(caseOfRemoveProduct);
+  const { settingAlertBox } = useAlertBox();
+  const showAlertBox = (getValue) => {
+    settingAlertBox(getValue); // 경고창 세팅
+  };
 
   const makeAlert = () => {
-    setAlertModalState(true); // 경고창을 띄운다
-    setDarkFilterState(true); // 다크필터를 띄운다
-    setAlertModalText('삭제하시겠습니까?'); // 텍스트를 설정한다
-    setAlertModalMoveState({
-      // 이동 여부를 설정한다
-      needToMove: false,
-      moveUrl: '',
-    });
-    setAlertModalUiType('confirmAndCancel'); // 버튼 타입을 설정한다.
-    setCaseOfRemoveProduct({
-      // 제품을 지울 생각이 있는 걸 알린다
-      needToRemove: true,
+    showAlertBox({
+      alertText: '삭제하시겠습니까?',
+      btnUiType: 'confirmAndCancel',
       product: product,
     });
   };
--- src/pages/SignUp/@hook/useConfirmEmail.js
+++ src/pages/SignUp/@hook/useConfirmEmail.js
@@ -1,29 +1,18 @@
-// import {
-//   alertModalState,
-//   alertModalText,
-//   alertModalMoveState,
-//   alertModalUiType,
-// } from '@/components/AlertBox/@recoil/alertModalState.js';
-//import { darkFilterState } from '@/store/darkFilterState.js';
 import { useCallback, useMemo } from 'react';
-import { checkValidation } from '@/utils';
+import { useRecoilState, useRecoilValue } from 'recoil';
 import { collection, getDocs, query, where } from 'firebase/firestore';
-import { db } from '@/firebase/app.js';
+
 import { signUpFormState, emailConfirmState } from '../@recoil/signUp';
-import { useRecoilState, useSetRecoilState, useRecoilValue } from 'recoil';
-import { useAlertBox } from '@/components/AlertBox/customHooks/useAlertBox.js';
+
+import { checkValidation } from '@/utils';
+import { db } from '@/firebase/app.js';
+import { useAlertBox } from '@/components/AlertBox/@hook/useAlertBox.js';
 
 // 이메일 중복 확인 로직
 export function useConfirmEmail() {
   const [emailConfirm, setEmailConfirm] = useRecoilState(emailConfirmState);
   const signUpForm = useRecoilValue(signUpFormState);
 
-  // 경고창
-  // const setAlertModalState = useSetRecoilState(alertModalState);
-  // const setAlertModalText = useSetRecoilState(alertModalText);
-  // const setAlertModalMoveState = useSetRecoilState(alertModalMoveState);
-  // const setAlertModalUiType = useSetRecoilState(alertModalUiType);
-  // const setDarkFilterState = useSetRecoilState(darkFilterState);
   const { settingAlertBox } = useAlertBox();
   const showAlertBox = (getValue) => {
     settingAlertBox(getValue); // 경고창 세팅
@@ -53,22 +42,9 @@ export function useConfirmEmail() {
           : '사용 가능한 이메일 입니다.';
       }
 
-      // 알림
-      // setAlertModalState(true);
-      // setDarkFilterState(true);
-      // setAlertModalText(alertPragment);
-      // setAlertModalMoveState({
-      //   needToMove: false,
-      //   moveUrl: '',
-      // });
-      // setAlertModalUiType('onlyConfirm');
       showAlertBox({
-        alertText: alertPragment, // (필수) [경고창 텍스트 설정]
-        needToMove: false, // (선택) [이동 여부 설정] 만약에 '확인'버튼을 눌렀을 때, 어디론가 페이지를 이동시키고 싶은 경우만 넣어주기
-        moveUrl: '', // (선택) [이동 할 url 설정] 만약에 '확인'버튼을 눌렀을 때, 어디론가 페이지를 이동시키고 싶은 경우만 넣어주기
-        btnUiType: 'onlyConfirm', // (필수) [버튼 형태 설정] 확인 버튼만 있는 'onlyConfirm' 또는, 확인/취소 버튼이 있는 'confirmAndCancel'을 넣어주기
-        needToRemove: false, // (선택) [장바구니에서 상품을 삭제할 여부 설정] 장바구니 페이지에서 상품을 삭제할 생각이 있다면 넣어주기
-        product: {}, // (선택) [어떤 상품을 삭제할 것인지 설정] 삭제할 상품 넣어주기
+        alertText: alertPragment, 
+        btnUiType: 'onlyConfirm', 
       });
     },
     [signUpForm]
