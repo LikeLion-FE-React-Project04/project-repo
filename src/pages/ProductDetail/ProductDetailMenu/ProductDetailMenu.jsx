1f7085c50263be4027d8d6406418c883924b440b
= <=>
2023-04-21 13:58:45 +0900

    ✨ [FEATURE] useMoveScroll 적용하여 네비게이션 구현 #225

--- src/pages/ProductDetail/ProductDetailMenu/ProductDetailMenu.jsx
+++ src/pages/ProductDetail/ProductDetailMenu/ProductDetailMenu.jsx
@@ -1,7 +1,8 @@
-import { useEffect } from 'react';
+import { useRef } from 'react';
+
 import styles from './ProductDetailMenu.module.scss';
 
-function ProductDetailMenu() {
+function ProductDetailMenu({ tabs }) {
   // useEffect(() => {
   //   window.addEventListener('scroll', handleScroll, { capture: true }); // 스크롤 이벤트 등록
   //   return () => {
@@ -16,7 +17,7 @@ function ProductDetailMenu() {
 
   //   // 스크롤의 실시간 위치
   //   const scrollTop = document.getElementById('app')?.scrollTop; // 최상단 div 기준으로 스크롤 위치를 감지
-      
+
   //     // 스크롤 위치가 tabRef(하위메뉴 탭)의 위치보다 아래이면
   //     if (scrollTop >= tabRef.current.offsetTop) {
   //       fixTab.current = true;   // fixTab 변수는 트루
@@ -27,34 +28,43 @@ function ProductDetailMenu() {
   //     // 스크롤 위치가 detailRef(하위메뉴 2번)의 위치보다 위이면
   //     if (scrollTop < detailRef.current.offsetTop - offset) {
   //       setTab(0); // 하위메뉴 탭은 자동으로 인덱스 0을 보여주자
-  //     } 
+  //     }
   //     // 스크롤 위치가 detailRef(하위메뉴 2번)의 위치이거나 아래이면
   //     else if (scrollTop >= detailRef.current.offsetTop - offset) {
   //       setTab(1); // 하위메뉴 탭은 자동으로 인덱스 0을 보여주자
-  //     } 
+  //     }
 
-    
   // }, [tabRef.current, detailRef.current]);
+
+  const naviRef = useRef([]);
+
+  function InitFocusing() {
+    naviRef.current.forEach((ref) => {
+      ref.classList.remove(styles.Active);
+    });
+  }
+
+  const menuTabs = Array.from(tabs).map((tab, index) => {
+    return (
+      // eslint-disable-next-line react/jsx-key
+      <li>
+        <button
+          ref={(el) => (naviRef.current[index] = el)}
+          onClick={(e) => {
+            InitFocusing();
+            e.target.classList.add(styles.Active);
+            tab.onMoveToElement();
+          }}
+        >
+          {tab.name}
+        </button>
+      </li>
+    );
+  });
+
   return (
     <div className={styles.productDetailMenu}>
-      <ul>
-        <li>
-          <button href="" className={styles.Active}>
-            상품설명
-          </button>
-        </li>
-        <li>
-          <button href="">상세정보</button>
-        </li>
-        <li>
-          <button href="">
-            후기 <span>(1,000)</span>
-          </button>
-        </li>
-        <li>
-          <button href="">문의</button>
-        </li>
-      </ul>
+      <ul>{menuTabs}</ul>
     </div>
   );
 }
