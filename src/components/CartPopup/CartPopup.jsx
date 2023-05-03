5a89e06c394989906b4ea1ccefe9c68bac4a07e2
= <=>
2023-05-03 13:19:27 +0900

    🎨 [REFACTORING] 애니메이션 추가 #296

--- src/components/CartPopup/CartPopup.jsx
+++ src/components/CartPopup/CartPopup.jsx
@@ -5,6 +5,7 @@ import { cartPopupInfoState, cartPopupState } from '../../store/Popup';
 import styles from './CartPopup.module.scss';
 import { useEffect, useState } from 'react';
 import { useRecoilValue } from 'recoil';
+import { motion, AnimatePresence } from 'framer-motion';
 
 function CartPopup({ type }) {
   const [cartPopup, setCartPopup] = useRecoilState(cartPopupState);
@@ -13,11 +14,11 @@ function CartPopup({ type }) {
   // 부드럽게 사라잘려면 opacity로 랜더링을 계속 해야하나?
   //const [opacity, setOpacity] = useState(100);
 
-  // 헤더랑 고정형 헤더를 따로 분리해서 만들어서 2가지 경우로 만들어야합니다..
+  // 헤더랑 고정형 헤더를 따로 분리해서 만들어서 2가지 경우로 만들어야합니다..ㅠ
   let layoutStyle;
-  
+
   if (type == 'originHeader') {
-    layoutStyle = 'cartPopup2'
+    layoutStyle = 'cartPopup2';
   } else {
     layoutStyle = 'cartPopup';
   }
@@ -37,18 +38,46 @@ function CartPopup({ type }) {
 
   if (cartPopup) {
     return (
-      <div className={styles[layoutStyle]}>
-        <div className={styles.cartInfo}>
-          <div
-            className={styles.img}
-            style={{
-              background: `url(${cartPopupInfo.img}) 50% 50% / 120% no-repeat`,
+      <AnimatePresence>
+        <div className={styles[layoutStyle]}>
+          <motion.div
+            className={styles.outside}
+            initial={{
+              opacity: 0,
+              scale: 0.75,
             }}
-          />
-          <div className={styles.productTitle}>{cartPopupInfo.desc}</div>
-          <div className={styles.phrases}>장바구니가 담겼습니다.</div>
+            animate={{
+              opacity: 1,
+              scale: 1,
+              transition: {
+                ease: 'easeOut',
+                duration: 0.15,
+              },
+            }}
+            exit={{
+              opacity: 0,
+              scale: 0.75,
+              transition: {
+                ease: 'easeIn',
+                duration: 0.15,
+              },
+            }}
+          >
+            <div className={styles.inside}>
+              <div className={styles.cartInfo}>
+                <div
+                  className={styles.img}
+                  style={{
+                    background: `url(${cartPopupInfo.img}) 50% 50% / 120% no-repeat`,
+                  }}
+                />
+                <div className={styles.productTitle}>{cartPopupInfo.desc}</div>
+                <div className={styles.phrases}>장바구니가 담겼습니다.</div>
+              </div>
+            </div>
+          </motion.div>
         </div>
-      </div>
+      </AnimatePresence>
     );
   }
 }
